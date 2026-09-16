d305275631da407117a93bc96222b5746a46326f
Tim Fletcher <timfletcher88@gmail.com>
2026-09-16 16:22:51 +1000

    Added a description preamble to CAP Feed Sources page

--- src/CapFeedSourceListBuilder.php
+++ src/CapFeedSourceListBuilder.php
@@ -4,11 +4,47 @@ namespace Drupal\cap_alerts_aggregator_connector;
 
 use Drupal\Core\Config\Entity\ConfigEntityListBuilder;
 use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Url;
 
 /**
  * Provides a listing of CAP Feed Source entities.
  */
 class CapFeedSourceListBuilder extends ConfigEntityListBuilder {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render() {
+    $build = parent::render();
+    $build['endpoint_info'] = $this->buildEndpointInfo();
+    $build['endpoint_info']['#weight'] = -10;
+    return $build;
+  }
+
+  /**
+   * Builds the explanatory markup shown above the feed sources table.
+   */
+  private function buildEndpointInfo(): array {
+    $endpointPath = Url::fromRoute('cap_alerts_aggregator_connector.api.feed_sources')->toString();
+    $settingsUrl = Url::fromRoute('cap_alerts_aggregator_connector.settings_form');
+
+    return [
+      '#type' => 'container',
+      '#attributes' => ['class' => ['cap-feed-source-endpoint-info']],
+      'path' => [
+        '#type' => 'markup',
+        '#markup' => '<p>' . $this->t('The CAP Aggregator reads this list from: <code>@path</code>', [
+          '@path' => $endpointPath,
+        ]) . '</p>',
+      ],
+      'explanation' => [
+        '#type' => 'markup',
+        '#markup' => '<p>' . $this->t('This endpoint can only be accessed by sending the correct shared secret in an <code>X-Cap-Aggregator-Secret</code> request header. Requests with a missing or incorrect secret receive a 403 response with no feed data. Configure the shared secret on the <a href=":settings_url">CAP Aggregator Connector settings</a> page.', [
+          ':settings_url' => $settingsUrl->toString(),
+        ]) . '</p>',
+      ],
+    ];
+  }
 
   /**
    * {@inheritdoc}
